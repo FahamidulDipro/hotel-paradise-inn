441f958221a5716e6d344af769b3ea0c416ff0b4
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-12 01:12:46 +0600

    sign in with google functionality added with react firebase hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-import useFirebase from '../../useFirebase';
 
 const Login = () => {
-    const {user,signInGoogle} = useFirebase(auth);
+    const [signInWithGoogle,user,error] = useSignInWithGoogle(auth);
+    const handleGoogleSignIn=()=>{
+        signInWithGoogle().then(()=>{
+            console.log(user);
+        })
+    }
     return (
         <div>
            <h1>Login</h1> 
-           <Button variant='primary' onClick={signInGoogle}>SignIn with Google</Button>
+           <Button variant='primary' onClick={()=>handleGoogleSignIn()}>SignIn with Google</Button>
         </div>
     );
 };
--- src/useFirebase.js
+++ src/useFirebase.js
@@ -1,19 +1,14 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+
 import { useState } from "react";
 import auth from "./firebase.init";
 
 const useFirebase = () => {
-  const [user, setUser] = useState({});
-  const provider = new GoogleAuthProvider();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   const signInGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-        console.log(user);
-      })
-      .catch((error) => console.log(error));
+    signInWithGoogle();
   };
 
   return { user, signInGoogle };
