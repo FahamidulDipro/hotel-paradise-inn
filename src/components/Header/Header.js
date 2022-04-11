5cfbc0d24d840cabd22949b2797bc3d5cd96a91c
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-12 01:30:09 +0600

    Showing username on navbar using useAuthState from react firebase hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Header = () => {
-    return (
-        <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Hotel Paradise Inn</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-       <Link to='/' className='text-decoration-none mx-3 text-secondary'>Home</Link>
-       <Link to='/rooms' className='text-decoration-none mx-3 text-secondary'>Rooms</Link>
-       <Link to='/checkout' className='text-decoration-none mx-3 text-secondary'>Checkout</Link>
-       <Link to='/register' className='text-decoration-none mx-3 text-secondary'>Register</Link>
-       <Link to='/login' className='text-decoration-none mx-3 text-secondary'>Login</Link>
-      
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-    );
+  const [user] = useAuthState(auth);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Hotel Paradise Inn</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="text-decoration-none mx-3 text-secondary">
+              Home
+            </Link>
+            <Link
+              to="/rooms"
+              className="text-decoration-none mx-3 text-secondary"
+            >
+              Rooms
+            </Link>
+            <Link
+              to="/checkout"
+              className="text-decoration-none mx-3 text-secondary"
+            >
+              Checkout
+            </Link>
+            <Link
+              to="/register"
+              className="text-decoration-none mx-3 text-secondary"
+            >
+              Register
+            </Link>
+            <Link
+              to="/login"
+              className="text-decoration-none mx-3 text-secondary"
+            >
+              Login
+            </Link>
+            <span>{user?.displayName}</span>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
